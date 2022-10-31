39617bad2acf729f3dd86c48d994f118e39e973f
Sultanabdulmmi <96442245+Sultanabdulmmi@users.noreply.github.com>
2022-10-31 22:47:07 +0700

    Add files via upload

---
+++ src/main/java/PWS/TakeHome1_24/TakeHome124Application.java
@@ -0,0 +1,13 @@
+package PWS.TakeHome1_24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeHome124Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeHome124Application.class, args);
+	}
+
+}
---
+++ src/test/java/PWS/TakeHome1_24/TakeHome124ApplicationTests.java
@@ -0,0 +1,13 @@
+package PWS.TakeHome1_24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeHome124ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
